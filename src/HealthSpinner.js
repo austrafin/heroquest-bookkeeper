d88d9e787e2df8c55e1e8f2805ab61e7354a8a74
Matti Syrjänen <mhsyrjanen@outlook.com>
2020-02-22 14:33:18 +0200

    Prevent other characters than digits to be entered in the HealthSpinner text field

--- src/HealthSpinner.js
+++ src/HealthSpinner.js
@@ -8,6 +8,24 @@ const HealthSpinner = ({ labelText }) => {
       label={labelText}
       type="number"
       inputProps={{ min: "0", max: "100", step: "1" }}
+      onKeyPress={evt => {
+        var charCode = evt.which ? evt.which : evt.keyCode;
+        if (
+          charCode > 47 &&
+          charCode < 58 &&
+          evt.target.value + evt.key <= 100
+        ) {
+          if (evt.target.value[0] === "0") {
+            if (charCode === 48) {
+              evt.preventDefault();
+            } else {
+              evt.target.value = evt.target.value.slice(1);
+            }
+          }
+        } else {
+          evt.preventDefault();
+        }
+      }}
     />
   );
 };
