3ff66df4ec85285f6583b4ff632e8710f9b40cef
Matti Syrjänen <mhsyrjanen@outlook.com>
2020-02-22 17:48:17 +0200

    Corrected NumberInput maxValue

--- src/NumberInput.js
+++ src/NumberInput.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { TextField } from "@material-ui/core";
 
-const NumberInput = ({ labelText, maxValue = "100", step = "1" }) => {
+const NumberInput = ({ labelText, maxValue = 100, step = 1 }) => {
   return (
     <TextField
       id="standard-basic"
       label={labelText}
       type="number"
-      inputProps={{ min: "0", maxValue, step }}
-      onKeyPress={evt => {
+      inputProps={{ min: 0, maxValue, step }}
+      onKeyPress={(evt, value = maxValue) => {
         var charCode = evt.which ? evt.which : evt.keyCode;
         if (
           charCode > 47 &&
           charCode < 58 &&
-          evt.target.value + evt.key <= 100
+          evt.target.value + evt.key <= value
         ) {
           if (evt.target.value[0] === "0") {
             if (charCode === 48) {
