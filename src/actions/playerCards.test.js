0246f1191c552d3bdc0ccb9f650d60ca36c8e087
Matti Syrjänen <mhsyrjanen@outlook.com>
2020-07-16 18:04:39 +0300

    Added playerCards action tests.

---
+++ src/actions/playerCards.test.js
@@ -0,0 +1,91 @@
+import * as actions from "../actions/playerCards";
+
+describe("playerCards actions", () => {
+  const cardId = "123";
+  const label = "label";
+  const values = {};
+  const selectedFile = null;
+
+  it("should create an action for adding a new player card", () => {
+    expect(actions.addPlayerCard(values)).toEqual({
+      type: actions.ADD,
+      values: values,
+    });
+  });
+
+  it("should create an action for deleting a player card", () => {
+    expect(actions.deletePlayerCard(cardId)).toEqual({
+      type: actions.DELETE,
+      cardId: cardId,
+    });
+  });
+
+  it("should create an action for updating player card base values", () => {
+    expect(actions.updateBaseValues(values, cardId)).toEqual({
+      type: actions.UPDATE_BASE_VALUES,
+      cardId: cardId,
+      values: values,
+    });
+  });
+
+  it("should create an action for loading the player cards.", () => {
+    expect(actions.loadPlayerCards()).toEqual({
+      type: actions.LOAD,
+    });
+  });
+
+  it("should create an action for settings the label values.", () => {
+    const value = "value";
+    expect(actions.setValue(value, label, cardId)).toEqual({
+      type: actions.SET_VALUE,
+      value: value,
+      label: label,
+      cardId: cardId,
+    });
+  });
+
+  it("should create an action for incrementing a label value.", () => {
+    const incrementValue = 1;
+    expect(actions.increment(incrementValue, label, cardId)).toEqual({
+      type: actions.INCREMENT,
+      incrementValue: incrementValue,
+      label: label,
+      cardId: cardId,
+    });
+  });
+
+  it("should create an action for decrementing a label value.", () => {
+    const decrementValue = 1;
+    expect(actions.decrement(decrementValue, label, cardId)).toEqual({
+      type: actions.DECREMENT,
+      incrementValue: decrementValue,
+      label: label,
+      cardId: cardId,
+    });
+  });
+
+  it("should create an action for setting the selected image file.", () => {
+    expect(actions.setSelectedImageFile(selectedFile, cardId)).toEqual({
+      type: actions.SET_SELECTED_IMAGE,
+      selectedFile: selectedFile,
+      cardId: cardId,
+    });
+  });
+
+  it("should create an action for uploading an image file.", () => {
+    expect(actions.uploadImage(selectedFile, cardId)).toEqual({
+      type: actions.UPLOAD_IMAGE,
+      selectedFile: selectedFile,
+      cardId: cardId,
+    });
+  });
+
+  it("should create an action for adding an armory item.", () => {
+    const itemId = "armory_123";
+    expect(actions.addArmoryItem(cardId, itemId)).toEqual({
+      type: actions.ADD_ARMORY_ITEM,
+      cardId: cardId,
+      itemId: itemId,
+    });
+  });
+});
